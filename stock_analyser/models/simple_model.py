ae07c5b73adba47433960536fbfec50b7ace896f
deepak.jayprakash <deepakjpkodlekere@gmail.com>
2020-07-04 14:43:02 +0530

    added preprocessing and plotting

--- stock_analyser/models/simple_model.py
+++ stock_analyser/models/simple_model.py
@@ -12,42 +12,51 @@ matplotlib.use('TkAgg')
 
 
 def simple_test():
-    sql = "select trade_date,close from time_series where company_id = %d and close is not null" % (4086)
+    sql = "select trade_date,close, num_shares from time_series where company_id = %d and close is not null" % (4086)
     company_data = pd.read_sql(sql, settings.DATABASE_URL)
-    print(company_data.head())
 
+    company_data_modified = pre_process_data(company_data)
+
+    print(company_data_modified.head())
     feature_train, feature_test, result_train, result_test = train_test_split(
-        company_data[['trade_date']], company_data['close'], test_size=0.2)
+        company_data_modified[['trade_date','date_int', 'num_shares']], company_data_modified['close'], test_size=0.2)
 
-    X_train = pd.DataFrame(columns=['trade_date'])
-    X_test = pd.DataFrame(columns=['trade_date'])
-    for i, row in feature_train.iterrows():
-        X_train.loc[i] = row['trade_date'].toordinal()
+    print("train size", feature_train.shape)
+    print("test size", feature_test.shape)
 
-    for i, row in feature_test.iterrows():
-        X_test.loc[i] = row['trade_date'].toordinal()
+    apply_linear_regression(feature_test, feature_train, result_test, result_train, company_data_modified)
+    response = {}
+    return JsonResponse(response)
 
-    print("train size", X_train.shape)
-    print("test size", X_test.shape)
 
+def apply_linear_regression(feature_test, feature_train, result_test, result_train, company_data):
+    X_train = feature_train[['date_int', 'num_shares']]
+    X_test = feature_test[['date_int', 'num_shares']]
     regr = linear_model.LinearRegression()
-
-    # print(type(X_train['trade_date'].get(0)))
-
     regr.fit(X_train, result_train)
     print("regression_done")
     print("score: ", regr.score(X_train, result_train))
     result_pred = regr.predict(X_test)
     print("mae", mean_absolute_error(result_test, result_pred))
-
     test = pd.DataFrame(columns=['actual', 'pred'])
     test['actual'] = result_test
     test['pred'] = result_pred
     print(test.tail(n=40))
 
-    # plt.scatter(company_data['trade_date'], company_data['close'], color='r')
-    plt.scatter(feature_test['trade_date'], result_test, color='r')
-    plt.scatter(feature_test['trade_date'], result_pred, color='b')
+    plot_results(company_data, feature_test, result_pred, result_test)
     plt.show()
-    response = {}
-    return JsonResponse(response)
+
+
+def plot_results(company_data, feature_test, result_pred, result_test):
+    ax = company_data.plot(x='trade_date', y='close', style='b-', grid=True)
+    ax.set_xlabel("date")
+    ax.set_ylabel("price")
+    ax.scatter(feature_test['trade_date'], result_test, color='r')
+    ax.scatter(feature_test['trade_date'], result_pred, color='g')
+    ax.legend(['actual_data', 'test', 'predicted'])
+
+
+def pre_process_data(company_data):
+    company_data_modified = company_data.copy()
+    company_data_modified['date_int'] = company_data.apply(lambda row: row['trade_date'].toordinal(), axis=1)
+    return company_data_modified
